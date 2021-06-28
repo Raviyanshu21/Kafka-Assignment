8f335a15850f5b7414e562a1ae33168ae861284f
Raviyanshu Singh <raviyanshu.singh@knoldus.com>
2021-06-28 22:52:10 +0530

    Added kafka assignment with readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Adding Kafka-Assignment
---
+++ src/main/scala/com/knoldus/Consumer.scala
@@ -0,0 +1,47 @@
+package com.knoldus
+
+import java.util.{ Collections, Properties}
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import java.io._
+
+import scala.collection.JavaConverters._
+
+object Consumer {
+
+  def main(args: Array[String]): Unit = {
+    consumeFromKafka("test")
+  }
+
+  def consumeFromKafka(topic: String) = {
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", classOf[CustomDeserializer])
+    props.put("auto.offset.reset", "latest")
+    props.put("group.id", "consumer-group")
+    println("inside consumer")
+    val consumer: KafkaConsumer[String, User] = new KafkaConsumer[String, User](props)
+    consumer.subscribe(Collections.singletonList(topic))
+    while (true) {
+      val record = consumer.poll(1000).asScala.iterator
+      def writeFile(filename:String, record:Seq[String]): Unit = {
+        val file = new File("/home/knoldus/Kafka-Assign/src/main/scala/com/knoldus/Consumer.txt")
+        val bw = new BufferedWriter(new FileWriter(file))
+        for (value <- record) {
+          bw.write(value)
+          println(value)
+        }
+        bw.close()
+      }
+    }
+
+
+  /*  val record = consumer.poll(1000).asScala.toList.map(_.value())
+    println("consumer record data is")
+    println(record)
+    println("consumer Done")
+    record
+*/
+  }
+}
+
